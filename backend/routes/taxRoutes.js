f210ffe966ca4e0b2237e2924f1ebd5557f04a7b
Suchit007-sk <kawadesuchit@gmail.com>
2025-09-05 11:05:29 +0530

    Add backend folder

---
+++ backend/routes/taxRoutes.js
@@ -0,0 +1,10 @@
+
+const express = require("express");
+const { calculateTax } = require("../controllers/taxController");
+
+const router = express.Router();
+
+// POST /api/tax/calculate
+router.post("/calculate", calculateTax);
+
+module.exports = router;
