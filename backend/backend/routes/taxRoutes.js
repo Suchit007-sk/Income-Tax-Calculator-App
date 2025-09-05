9cf102fdb8f50c02539fd3c9fba754dbd69368e6
Suchit007-sk <kawadesuchit@gmail.com>
2025-09-05 10:56:14 +0530

    Remove backend folder from repo but keep locally

--- backend/backend/routes/taxRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { calculateTax } = require("../controllers/taxController");
-
-const router = express.Router();
-
-// POST /api/tax/calculate
-router.post("/calculate", calculateTax);
-
-module.exports = router;
